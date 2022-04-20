ef9061b38d351cec6641907646bb93ac5a17485c
shihab shumon <shumonshihab62@gmail.com>
2022-04-20 14:05:40 +0600

    customized the dashboard drawer

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
+import {
+  Box,
+  AppBar,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import {
   Switch,
   Route,
@@ -15,7 +17,6 @@ import {
   NavLink,
   useHistory,
 } from "react-router-dom";
-import { Button } from "@mui/material";
 import Pay from "../Pay/Pay";
 import MyOrders from "../MyOrders/MyOrders";
 import Review from "../Review/Review";
@@ -26,7 +27,14 @@ import AddProduct from "../ForAdmin/AddProduct/AddProduct";
 import MakeAdmin from "../ForAdmin/MakeAdmin/MakeAdmin";
 import ManageProducts from "../ForAdmin/ManageProducts/ManageProducts";
 import "./Dashboard.css";
-const drawerWidth = 240;
+import { BiHomeAlt } from "react-icons/bi";
+import { GrNotes, GrUserAdmin } from "react-icons/gr";
+import { CgPlayListAdd } from "react-icons/cg";
+import { BiBox } from "react-icons/bi";
+import { BsBoxArrowInRight } from "react-icons/bs";
+import { MdOutlineReviews } from "react-icons/md";
+
+const drawerWidth = 250;
 
 function Dashboard(props) {
   let { path, url } = useRouteMatch();
@@ -43,68 +51,68 @@ function Dashboard(props) {
     padding: "auto",
   };
   const dashboardButton = {
-    fontSize: "15px",
-    color: "#000",
-    fontWeight: "bold",
-    margin: "8px 0",
     fontFamily: "var(--nunito-font)",
   };
   const drawer = (
     <Box
       sx={{
         height: "100%",
-        background: "var(--dashboard-bg)",
+        background: "linear-gradient(195deg, rgb(66, 66, 74), rgb(25, 25, 25))",
         color: "white",
-        display: "flex",
-        justifyContent: "center",
       }}
       className="dashboard-drawer"
     >
-      <Toolbar />
       {user?.email && admin ? (
         <Box
           sx={{
             fontFamily: "var(--dosis-font)",
             display: "flex",
             flexDirection: "column",
-            ml: -8,
+            width: "100%",
             mt: 25,
             textTransform: "none",
           }}
         >
-          <NavLink style={dashboardLink} to={`/`}>
-            <Button color="inherit" style={dashboardButton}>
-              Home
-            </Button>
+          <NavLink style={dashboardLink} to={`/`} className="dashboard-link">
+            <BiHomeAlt className="dashboard-link-icon" /> Home
           </NavLink>
-          <NavLink style={dashboardLink} to={`${url}/manageAllOrders`}>
-            <Button color="inherit" style={dashboardButton}>
-              Manage All Orders
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/manageAllOrders`}
+            className="dashboard-link"
+          >
+            <GrNotes className="dashboard-link-icon" /> All Orders
           </NavLink>
-          <NavLink style={dashboardLink} to={`${url}/AddProduct`}>
-            <Button color="inherit" style={dashboardButton}>
-              Add a product
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/AddProduct`}
+            className="dashboard-link"
+          >
+            <CgPlayListAdd className="dashboard-link-icon" /> Add product
           </NavLink>
-          <NavLink style={dashboardLink} to={`${url}/makeAdmin`}>
-            <Button color="inherit" style={dashboardButton}>
-              Make Admin
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/makeAdmin`}
+            className="dashboard-link"
+          >
+            <GrUserAdmin className="dashboard-link-icon" /> Make Admin
           </NavLink>
-          <NavLink style={dashboardLink} to={`${url}/manageProducts`}>
-            <Button color="inherit" style={dashboardButton}>
-              Manage Products
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/manageProducts`}
+            className="dashboard-link"
+          >
+            <BiBox className="dashboard-link-icon" /> Products
           </NavLink>
-          <Button
+          <button
             style={dashboardButton}
             onClick={() => {
               signOutUser(history);
             }}
+            className="dashboard-link"
           >
-            Log Out
-          </Button>
+            <BsBoxArrowInRight className="dashboard-link-icon" /> Log Out
+          </button>
         </Box>
       ) : (
         <Box
@@ -117,35 +125,32 @@ function Dashboard(props) {
             textTransform: "none",
           }}
         >
-          <NavLink style={dashboardLink} to={`/`}>
-            <Button color="inherit" style={dashboardButton}>
-              Home
-            </Button>
+          <NavLink style={dashboardLink} to={`/`} className="dashboard-link">
+            <BiHomeAlt /> Home
           </NavLink>
-          <NavLink style={dashboardLink} to={`${url}/myOrders`}>
-            <Button color="inherit" style={dashboardButton}>
-              My Orders
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/myOrders`}
+            className="dashboard-link"
+          >
+            <GrNotes className="dashboard-link-icon" /> My Orders
           </NavLink>
-          {/* <NavLink style={dashboardLink} to={`${url}/pay`}>
-            <Button color="inherit" style={dashboardButton}>
-              Pay
-            </Button>
-          </NavLink> */}
-          <NavLink style={dashboardLink} to={`${url}/review`}>
-            <Button color="inherit" style={dashboardButton}>
-              Review
-            </Button>
+          <NavLink
+            style={dashboardLink}
+            to={`${url}/review`}
+            className="dashboard-link"
+          >
+            <MdOutlineReviews className="dashboard-link-icon" /> Review
           </NavLink>
-          <Button
-            color="inherit"
+          <button
             style={dashboardButton}
+            className="dashboard-link"
             onClick={() => {
               signOutUser(history);
             }}
           >
-            Log Out
-          </Button>
+            <BsBoxArrowInRight className="dashboard-link-icon" /> Log Out
+          </button>
         </Box>
       )}
     </Box>
@@ -158,10 +163,13 @@ function Dashboard(props) {
       <AppBar
         position="fixed"
         sx={{
-          background: "var(--dashboard-bg)",
-          boxShadow: "var(--box-shadow)",
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
+          background: "rgb(240, 242, 245)",
+          boxShadow: "none",
+          borderRadius: { sm: "15px" },
+          width: { sm: `calc(100% - ${drawerWidth + 30}px)` },
+          ml: { sm: `${drawerWidth + 15}px` },
+          mr: { sm: "15px" },
+          mt: { sm: "15px" },
         }}
       >
         <Toolbar>
